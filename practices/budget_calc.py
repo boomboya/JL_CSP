eb8702c19d556e071de9c334db1cb14ac2d77888
boomboya <jovie.lineback@ucas-edu.net>
2025-09-07 03:45:51 +0000

    update bug cal

--- practices/budget_calc.py
+++ practices/budget_calc.py
@@ -8,5 +8,10 @@ num_three = int(input("tell me your utilities cost:\n"))
 num_four = int(input("tell me how much your groceries cost:\n"))
 num_five = int(input("tell me how much your transportation cost:\n"))
 
-income = num_one + num_two + num_three + num_four + num_five
+income = num_one 
+expenses = num_two + num_three + num_four + num_five
+print("Your income is:", income)
 savings = income*.1
+print("You should put this much of your income into savings:", savings)
+spending = income-(expenses+savings)
+print("you have this much spending money:", spending)
