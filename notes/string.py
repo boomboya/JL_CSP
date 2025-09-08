dd7ac4b89e1489574ecddf375279df921f51d05d
boomboya <jovie.lineback@ucas-edu.net>
2025-09-08 14:41:57 -0600

    String notes

--- notes/string.py
+++ notes/string.py
@@ -1,3 +1,16 @@
 # JL 7th String notes
 
 print("I did it")
+ #a set of symbols held together by quatation marks "content of string" or 'contnt of string'
+    #examples
+first_name = input("what is your first name:\n").strip().title()
+last_name = input("what is your last name:\n").strip().title()
+sentence = '          the quick brown fox jumps over the lazy dog.     '
+print(sentence.strip())
+
+full_name = first_name + " " + last_name
+
+print("welcome to my program", full_name + "!")
+
+   #takes two strings together and gets rid of 
+
