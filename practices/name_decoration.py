a1faaa1473d5becbf53f3623251fc42830a44f70
boomboya <jovie.lineback@ucas-edu.net>
2025-09-10 14:25:23 -0600

    Name decorator

---
+++ practices/name_decoration.py
@@ -0,0 +1,7 @@
+# JL 7th name decorator 
+
+name = input("Hi I am a name decorator! what is your first name:\n").strip().title()
+
+print("welcome", name)
+
+print("this is your decorated name", "<<<" + name + ">>>")
