64c5f099298cc6c18f4b9ee8e59cfd0fe96eb833
boomboya <jovie.lineback@ucas-edu.net>
2025-09-10 14:41:41 -0600

    silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,11 @@
+# JL 7th Silly sentences
+
+name = input("give me a name:\n").strip().title()
+
+verb = input("give me a past tense verb:\n")
+
+number = input("give me a any number:\n")
+
+print(name + " " + verb + " " + "from" + " " + number + " " + "meatballs.")
+
+
