4c24b135f098ab412452096a87d676bc5a6535b6
boomboya <jovie.lineback@ucas-edu.net>
2025-09-16 14:17:08 -0600

    finished updated hello world

---
+++ practices/updated_helloworld.py
@@ -0,0 +1,14 @@
+# JL 7th updated hello world 
+
+def welcome():
+    name = input("what is your name?")
+    print(f"hello, {name}!")
+
+welcome()
+welcome()
+welcome()
+welcome()
+welcome()
+
+farewell = "bye everyone"
+print(farewell)
