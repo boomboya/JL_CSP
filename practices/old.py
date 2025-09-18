c4b6d3b44067e522a369af5a90965a4d1a8523e1
boomboya <jovie.lineback@ucas-edu.net>
2025-09-18 14:29:58 -0600

    old enough

---
+++ practices/old.py
@@ -0,0 +1,12 @@
+# JL 7th old enough python
+
+age = int(input("what is your age?: "))
+
+if age >= 18:
+    print(f"you are {age}yrs old, you can vote")
+elif age >= 16:
+     print(f"you are {age}yrs old you can drive")
+elif age >= 15:
+    print(f"you are {age}yrs old you can get a learners permit")
+elif age >= 4:
+    print(f"you are {age}yrs old go to school")
