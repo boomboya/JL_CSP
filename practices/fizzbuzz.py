6c9c6e8e65751f0676ce47eb3031588136eb9702
boomboya <jovie.lineback@ucas-edu.net>
2025-09-23 14:23:51 -0600

    finished fizzbuzz

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -3,12 +3,12 @@
 x = 1
 
 while x < 51:
-    if x % 3 and x % 5 == 0:
+    if x % 3 ==0 and x % 5 == 0:
         print(f"fizzbuzz")
-    elif x % 3 == 0:
-        print(f"fizz")
     elif x % 5 == 0:
-        print(f"buzz")    
+        print(f"buzz")
+    elif x % 3 == 0:
+        print(f"fizz")    
     else:
         print(x)
     x+= 1
