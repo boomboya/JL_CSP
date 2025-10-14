719336eb02df535b20ab36e636589a01c2386132
boomboya <jovie.lineback@ucas-edu.net>
2025-10-14 14:41:01 -0600

    fizzbuzz

---
+++ c_programming/practices/fizzbuzz.c
@@ -0,0 +1,15 @@
+//JL 7th fizzbuzz
+#include <stdio.h>
+
+x = 1
+
+//while x < 51:
+    //if x % 3 ==0 and x % 5 == 0:
+        //print(f"fizzbuzz")
+    //elif x % 5 == 0:
+      //  print(f"buzz")
+    //elif x % 3 == 0:
+       // print(f"fizz")    
+    //else:
+       // print(x)
+    //x+= 1
